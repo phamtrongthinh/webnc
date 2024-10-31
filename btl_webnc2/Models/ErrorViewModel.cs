6ebfdc91cdfce2d0b3f5c9511e38e4826dbfa560
phamtrongthinh <[200~>
2024-10-31 21:19:49 +0700

    remove project

--- btl_webnc2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace btl_webnc2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
